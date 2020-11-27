9ac123b6fe0e213a3865a5f7669ff1fdc9b471a7
Strishwork <71441752+Strishwork@users.noreply.github.com>
2020-11-27 17:25:37 +0200

    Changes.

--- api/src/test/java/com/example/api/CountriesApiTest.kt
+++ api/src/test/java/com/example/api/CountriesApiTest.kt
@@ -1,34 +1,23 @@
 package com.example.api
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.api.toInput
 import com.apollographql.apollo.rx2.rxQuery
 import com.example.GetCountriesQuery
 import com.example.GetCountryByIdQuery
-import org.junit.Rule
 import org.junit.Test
 import org.mockito.Answers
 import org.mockito.ArgumentCaptor
 import org.mockito.Mockito.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class CountriesApiTest {
-
-    @Rule
-    @JvmField
-    val instantExecutorRule = InstantTaskExecutorRule()
 
     private val apolloClientMock = mock(ApolloClient::class.java, Answers.RETURNS_DEEP_STUBS)
 
+    private val countriesApi: CountriesApi = mock(CountriesApi::class.java)
+
     @Test
     fun test_apolloClientGetCountries() {
-        val countriesApi = spy(CountriesApi(apolloClientMock))
-
         val getCountriesQuery = GetCountriesQuery()
 
         doReturn(apolloClientMock.rxQuery(getCountriesQuery)).`when`(countriesApi).getCountries()
@@ -40,11 +29,10 @@ class CountriesApiTest {
 
     @Test
     fun test_apolloClientGetCountryById() {
-        val countriesApi = spy(CountriesApi(apolloClientMock))
-
         val getCountryByIdQuery = GetCountryByIdQuery("3".toInput())
 
-        doReturn(apolloClientMock.rxQuery(getCountryByIdQuery)).`when`(countriesApi).getCountryById("3")
+        doReturn(apolloClientMock.rxQuery(getCountryByIdQuery)).`when`(countriesApi)
+            .getCountryById("3")
         countriesApi.getCountryById("3")
 
         val captor = ArgumentCaptor.forClass(GetCountryByIdQuery::class.java)
