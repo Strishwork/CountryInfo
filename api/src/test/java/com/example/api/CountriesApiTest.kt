a6505bee8b488c54919d313bdf20e55415bc7aaf
Yevgen Oliinykov <v-yoliinykov@vrbo.com>
2020-11-27 17:55:45 +0200

    [INTERN-4] Refactored tests.

--- api/src/test/java/com/example/api/CountriesApiTest.kt
+++ api/src/test/java/com/example/api/CountriesApiTest.kt
@@ -1,8 +1,6 @@
 package com.example.api
 
 import com.apollographql.apollo.ApolloClient
-import com.apollographql.apollo.api.toInput
-import com.apollographql.apollo.rx2.rxQuery
 import com.example.GetCountriesQuery
 import com.example.GetCountryByIdQuery
 import org.junit.Test
@@ -14,30 +12,22 @@ class CountriesApiTest {
 
     private val apolloClientMock = mock(ApolloClient::class.java, Answers.RETURNS_DEEP_STUBS)
 
-    private val countriesApi: CountriesApi = mock(CountriesApi::class.java)
+    private val countriesApi: CountriesApi = CountriesApi(apolloClientMock)
 
     @Test
     fun test_apolloClientGetCountries() {
-        val getCountriesQuery = GetCountriesQuery()
-
-        doReturn(apolloClientMock.rxQuery(getCountriesQuery)).`when`(countriesApi).getCountries()
         countriesApi.getCountries()
 
-        verify(countriesApi).getCountries()
-        verify(apolloClientMock).query(getCountriesQuery)
+        verify(apolloClientMock).query(isA(GetCountriesQuery::class.java))
     }
 
     @Test
     fun test_apolloClientGetCountryById() {
-        val getCountryByIdQuery = GetCountryByIdQuery("3".toInput())
-
-        doReturn(apolloClientMock.rxQuery(getCountryByIdQuery)).`when`(countriesApi)
-            .getCountryById("3")
         countriesApi.getCountryById("3")
 
         val captor = ArgumentCaptor.forClass(GetCountryByIdQuery::class.java)
-        verify(countriesApi).getCountryById("3")
         verify(apolloClientMock).query(captor.capture())
-        assert(captor.allValues.last().id == "3".toInput())
+        assert(captor.allValues.last().id.value == "3")
     }
+    
 }
